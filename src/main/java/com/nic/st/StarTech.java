7850814109db03d44f4922736bc01ca156d97f94
AFlyingGrayson <theflyinggrayson01@gmail.com>
2018-03-31 16:40:06 -0500

    Initial Commit

---
+++ src/main/java/com/nic/st/StarTech.java
@@ -0,0 +1,25 @@
+package com.nic.st;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = StarTech.MODID, name = StarTech.NAME, version = StarTech.VERSION)
+public class StarTech
+{
+	public static final String MODID = "star-tech";
+	public static final String NAME = "Star Tech, Man! The Legendary Mod?";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+
+	}
+}
